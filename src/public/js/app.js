0c0c3f022b45253565e29fef046299f92f0d2c19
hojncode <glicm12@gmail.com>
2022-12-28 19:00:44 +0900

    Room Notifications

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -16,11 +16,23 @@ function addMessage(message) {
   ul.appendChild(li);
 }
 
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
+}
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -38,6 +50,13 @@ form.addEventListener("submit", handleRoomSubmit);
 socket.on("welcome", () => {
   addMessage("Someone Joined.");
 });
+
+socket.on("bye", () => {
+  addMessage("Someone left.");
+});
+
+socket.on("new_message", addMessage);
+
 //! Legacy : webSocket
 // const socket = new WebSocket(`ws://${window.location.host}`);
 // const messageList = document.querySelector("ul");
